ab8ba44375cf32ed94fb9dc1d255aa9060716311
Geovane Fedrecheski <geonnave@gmail.com>
2024-10-17 15:10:45 +0200

    feat: blink lend works!!

--- main.c
+++ main.c
@@ -1,24 +1,29 @@
+#include <stdint.h>
 
-// just to test the placement of variables in memory
-char var1;
-char var2 = 0;
-char var3 = 3;
-const char const1;
-const char const2 = 0;
-const char const3 = 5;
+#define GPIO_BASE        0x50000000U
+#define GPIO_OUT         (*(volatile unsigned int *)(GPIO_BASE + 0x504))  // GPIO output register
+#define GPIO_OUTSET      (*(volatile unsigned int *)(GPIO_BASE + 0x508))  // GPIO output set register
+#define GPIO_OUTCLR      (*(volatile unsigned int *)(GPIO_BASE + 0x50C))  // GPIO output clear register
+#define GPIO_DIR         (*(volatile unsigned int *)(GPIO_BASE + 0x514))  // GPIO direction register
+
+#define LED_PIN          13  // LED1 on nRF52840 DK (P0.13)
+
+void delay(volatile uint32_t duration) {
+    while (duration--);
+}
 
 int main(void) {
-    // classic FizzBuzz test
-    for (int i = 1; i <= 100; i++) {
-        if (i % var3 == var2 && i % const3 == var2) {
-            // FizzBuzz
-        } else if (i % var3 == var2) {
-            // Fizz
-        } else if (i % const3 == var2) {
-            // Buzz
-        } else {
-            // i
-        }
+    // Set LED_PIN (P0.13) as output
+    GPIO_DIR |= (1 << LED_PIN);
+
+    while (1) {
+        // Turn the LED on
+        GPIO_OUTCLR = (1 << LED_PIN);  // Clear bit to turn on (assuming active-low LED)
+        delay(1000000);
+
+        // Turn the LED off
+        GPIO_OUTSET = (1 << LED_PIN);  // Set bit to turn off
+        delay(1000000);
     }
 
     for (;;);
